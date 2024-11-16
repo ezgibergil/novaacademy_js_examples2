4a2c9ee153baad8280ccf6a49917353666604fd6
Ezgi Bergil <ezgibergil@Ezgi-Dizustu-Bilgisayar.local>
2024-11-16 18:02:45 +0300

    first commit

---
+++ ornekler.js
@@ -0,0 +1,104 @@
+/*let not=85;
+if(not>80){
+    console.log("Başarılı");
+}
+else{
+    console.log("Başarısız");
+}
+    
+
+let gun=3;
+switch(gun){
+    case 1:
+        console.log("Pazartesi");
+        break;
+    case 2:
+        console.log("Salı");
+        break;
+    case 3:
+        console.log("Çarşamba");
+        break;
+    default:
+        console.log("Hafta içi");
+}
+        
+
+let s1=4;
+let s2=5;
+let s3=6;
+
+ortalama = (s1+s2+s3)/3;
+console.log("Sayıların ortalaması:",ortalama);
+
+let s1=4;
+let s2=5;
+let s3=6;
+if (s1>s2 && s1>s3){
+    console.log("En büyük sayı: " ,s1);
+}
+else if (s2>s1 && s2>s3){
+    console.log("En büyük sayı: ",s2);
+}
+else{
+    console.log("En büyük sayı: ",s3);
+}
+
+let yas=18;
+if(yas>=18){
+    console.log("Reşitsiniz");
+}
+else{
+    console.log("Reşit değilsiniz");
+}
+    
+
+let maas = 20000;
+let yenimaas = maas*1.2;
+console.log("Yeni maaşınız: ",yenimaas);
+
+
+let gun = 1;
+switch(gun){
+    case 1:
+        console.log("Pazartesi");
+        break;
+    case 2:
+        console.log("Salı");
+        break;
+    case 3:
+        console.log("Çarşamba");
+        break;
+    case 4:
+        console.log("Perşembe");
+        break;
+    case 5:
+        console.log("Cuma");
+        break;
+    case 6:
+        console.log("Cumartesi");
+        break;
+    case 7:
+        console.log("Pazar");
+        break;
+    default:
+        console.log("Lütfen 1-7 arası bir rakam giriniz!");
+}
+        
+
+let sayi = 3;
+if (sayi%2==0){
+    console.log("Girdiğiniz sayı çift.");
+}
+else{
+    console.log("Girdiğiniz sayı tektir.");
+}
+
+*/
+let yas = 18;
+let maas = 25000;
+if (yas>=18 && maas>=5000){
+    console.log("Kredi alabilirsiniz");
+}
+else{
+    console.log("Krediye uygun değilsiniz");
+}
