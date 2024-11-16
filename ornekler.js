8b063e9d68f41185933c004019c7769b413734ee
Ezgi Bergil <ezgibergil@Ezgi-Dizustu-Bilgisayar.local>
2024-11-16 18:06:32 +0300

    first commit

--- ornekler.js
+++ ornekler.js
@@ -1,28 +1,5 @@
-/*let not=85;
-if(not>80){
-    console.log("Başarılı");
-}
-else{
-    console.log("Başarısız");
-}
-    
-
-let gun=3;
-switch(gun){
-    case 1:
-        console.log("Pazartesi");
-        break;
-    case 2:
-        console.log("Salı");
-        break;
-    case 3:
-        console.log("Çarşamba");
-        break;
-    default:
-        console.log("Hafta içi");
-}
-        
-
+/*
+#3 sayının ortalamasını bulup ekranda çıktı veren kodları yazınız.
 let s1=4;
 let s2=5;
 let s3=6;
@@ -30,6 +7,7 @@ let s3=6;
 ortalama = (s1+s2+s3)/3;
 console.log("Sayıların ortalaması:",ortalama);
 
+#3 sayıdan en büyük olan sayıyı bulan kodları yazınız.
 let s1=4;
 let s2=5;
 let s3=6;
@@ -43,6 +21,7 @@ else{
     console.log("En büyük sayı: ",s3);
 }
 
+#Reşit durumunu kontrol eden kodları yazınız.
 let yas=18;
 if(yas>=18){
     console.log("Reşitsiniz");
@@ -51,12 +30,12 @@ else{
     console.log("Reşit değilsiniz");
 }
     
-
+#Maaşa %20 zam yapan kodları yazınız.
 let maas = 20000;
 let yenimaas = maas*1.2;
 console.log("Yeni maaşınız: ",yenimaas);
 
-
+#Girilen rakama göre hangi güne aie olduğunu yazan programı yazınız.
 let gun = 1;
 switch(gun){
     case 1:
@@ -84,7 +63,7 @@ switch(gun){
         console.log("Lütfen 1-7 arası bir rakam giriniz!");
 }
         
-
+#Sayının tek mi çift mi olduğunu kontrol eden kodları yazınız.
 let sayi = 3;
 if (sayi%2==0){
     console.log("Girdiğiniz sayı çift.");
@@ -93,7 +72,8 @@ else{
     console.log("Girdiğiniz sayı tektir.");
 }
 
-*/
+
+#Maaşı 5000 den yüksek ve yaşı 18 den büyük olan kişilerin kredi durumunu kontrol eden kodları yazınız.
 let yas = 18;
 let maas = 25000;
 if (yas>=18 && maas>=5000){
@@ -102,3 +82,19 @@ if (yas>=18 && maas>=5000){
 else{
     console.log("Krediye uygun değilsiniz");
 }
+   
+#Bir öğrencinin notunu kontrol ederek geçip geçmediğini belirleyin.
+50'nin üzerindeyse "Geçti"
+50'nin altındaysa "Kaldı"
+
+let not = 55;
+if (not>=50){
+    console.log("Geçti");
+}
+else if (not<50){
+    console.log("Kaldı")
+}
+else{
+    console.log("Lütfen geçerli bir not giriniz.")
+}
+    */
